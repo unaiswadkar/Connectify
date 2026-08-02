7e729b415e1605867cf6710391b0556a843a12c4
unais <unaiswadkar1@gmail.com>
2026-08-03 00:44:03 +0530

    origin-changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
+      "http://localhost:5173",
       "http://localhost:5174",
       "https://connectify-frontend-zknz.onrender.com",
     ],
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,13 +8,13 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
+      "http://localhost:5173",
       "http://localhost:5174",
       "https://connectify-frontend-zknz.onrender.com",
     ],
     credentials: true,
   },
 });
-
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
