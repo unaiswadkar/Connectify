18e543d2d3d8156f02cf06041ea91571d3b925d4
unais <unaiswadkar1@gmail.com>
2026-08-02 23:44:29 +0530

    frontend-render-port-in-backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5174", credentials: true }));
+app.use(
+  cors({
+    origin: "https://connectify-frontend-zknz.onrender.com",
+    credentials: true,
+  }),
+);
 
 //routes
 app.use("/api/auth", authRoutes);
